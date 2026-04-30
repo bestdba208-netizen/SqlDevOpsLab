62119f1e970ca942077b34f155dca4c0f65cb0c4
Jeremiah Hale <bestdba208@gmail.com>
2026-04-30 00:48:34 -0600

    added files

---
+++ Scripts/DemoQueries.sql
@@ -0,0 +1,9 @@
+﻿-- Top users by reputation
+SELECT TOP 10 DisplayName, Reputation
+FROM dbo.Users
+ORDER BY Reputation DESC;
+
+-- Top posts
+SELECT TOP 10 Title, Score
+FROM dbo.Posts
+ORDER BY Score DESC;

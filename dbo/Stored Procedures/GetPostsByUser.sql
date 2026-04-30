8bfedc905fbb4202672dd0c4ef3ef864afe5b155
Admin <Admin@DESKTOP-EL4PPIU>
2026-04-29 23:28:12 -0600

    Add project files.

---
+++ dbo/Stored Procedures/GetPostsByUser.sql
@@ -0,0 +1,12 @@
+﻿CREATE   PROCEDURE dbo.GetPostsByUser
+    @UserId INT
+AS
+BEGIN
+    SELECT
+        Id,
+        CreationDate,
+        Score,
+        OwnerUserId
+    FROM dbo.Posts
+    WHERE OwnerUserId = @UserId;
+END
